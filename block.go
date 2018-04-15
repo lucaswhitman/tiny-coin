0f162773608c9a0563998b1902beaac1b57985ef
Lucas Whitman <lwhitman@levelsbeyond.com>
2018-04-14 22:36:47 -0600

    more additions

--- block.go
+++ block.go
@@ -1,1 +1,29 @@
 package coin
+
+import (
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+const Size = 32
+
+type Block struct {
+	Index        int
+	TimeStamp    time.Time
+	Data         string
+	PreviousHash [Size]byte
+	Hash         [Size]byte
+}
+
+func NewBlock(index int, timeStamp time.Time, data string, previousHash [Size]byte) *Block {
+	return &Block{index, timeStamp, data, previousHash, HashBlock(index, timeStamp, data, previousHash)}
+}
+
+func HashBlock(index int, timeStamp time.Time, data string, previousHash [Size]byte) [Size]byte {
+	sha := sha256.Sum256([]byte(strconv.Itoa(index) +
+		timeStamp.String() +
+		data +
+		string(previousHash[:])))
+	return sha
+}
--- blockchain.go
+++ blockchain.go
@@ -1,29 +1,24 @@
 package coin
 
-import (
-	"crypto/sha256"
-	"strconv"
-	"time"
-)
+import "fmt"
 
-const Size = 32
+func main() {
+	// Create the blockchain and add the genesis block
+	blockchain := make([]Block, 3)
+	blockchain = append(blockchain, CreateGenesisBlock())
+	previousBlock = blockchain[0]
 
-type Block struct {
-	Index        int
-	TimeStamp    time.Time
-	Data         string
-	PreviousHash [Size]byte
-	Hash         [Size]byte
-}
+	// How many blocks should we add to the chain
+	// after the genesis block
+	blocksToAdd = 20
 
-func NewBlock(index int, timeStamp time.Time, data string, previousHash [Size]byte) *Block {
-	return &Block{index, timeStamp, data, previousHash, HashBlock(index, timeStamp, data, previousHash)}
-}
-
-func HashBlock(index int, timeStamp time.Time, data string, previousHash [Size]byte) [Size]byte {
-	sha := sha256.Sum256([]byte(strconv.Itoa(index) +
-		timeStamp.String() +
-		data +
-		string(previousHash[:])))
-	return sha
+	// Add blocks to the chain
+	for i := 0; i < blocksToAdd; i++ {
+		blockToAdd = NextBlock(previousBlock)
+		blockchain = append(blockchain, blockToAdd)
+		previousBlock = blockToAdd
+		// Tell everyone about it!
+		fmt.Println("Block #{} has been added to the blockchain!".format(blockToAdd.index))
+		fmt.Println("Hash: {}\n".format(blockToAdd.hash))
+	}
 }
